73f3b38f4152ba207ec8a70424d8d9ec2af13089
lucasmquinteros <lucasmartiniquinteros@gmail.com>
2025-10-01 14:22:25 -0300

    Una pequeña implementacion del strategy en analysisController

--- src/controllers/AnalysisController.ts
+++ src/controllers/AnalysisController.ts
@@ -1,28 +1,43 @@
-import * as service from "../services/MarketAnalysisService/MarketAnalysisService";
+import { MarketAnalysisService } from "../services/MarketAnalysisService/MarketAnalysisService";
 import { ResponseService } from "../services/ResponseService";
 import { Request, Response } from "express";
-
+import { RiskGenerator } from "../services/MarketAnalysisService/RiskGenerator";
 // Controlador de análisis
 export class AnalysisController {
-        private static service: MarketAnalysisService = ;
+  private static service: MarketAnalysisService = new MarketAnalysisService();
 
-    static async getRiskAnalysis(req: Request, res: Response) {
-        try {
-            const user = req.user;
-            const riskAnalysis = AnalysisController.service.analyzePortfolioRisk(user.id);
-            ResponseService.ok(res, { riskAnalysis });
-        } catch (error) {
-            ResponseService.internalError(res, error, "Error al realizar el analisis de riesgo");
-        }
+  static async getRiskAnalysis(req: Request, res: Response) {
+    try {
+      const user = req.user;
+      const riskAnalysis = this.service.analyzePortfolioRisk(user.id);
+      ResponseService.ok(res, { riskAnalysis });
+    } catch (error) {
+      ResponseService.internalError(
+        res,
+        error,
+        "Error al realizar el analisis de riesgo"
+      );
     }
+  }
 
-    static async getRecommendations(req: Request, res: Response) {
-        try {
-            const user = req.user;
-            const recommendations = AnalysisController.service.generateInvestmentRecommendations(user.id);
-            ResponseService.ok(res, { recommendations });
-        } catch (error) {
-            ResponseService.internalError(res, error, "Error al generar recomendaciones");
-        }
+  static async getRecommendations(req: Request, res: Response) {
+    try {
+      const user = req.user;
+      const strategyForRisk = RiskGenerator.getStrategyClass(user.riskLevel);
+      const recommendationGenerator = new RiskGenerator(strategyForRisk);
+      const recommendations = recommendationGenerator.executeStrategy(
+        user.diversificationScore,
+        user.volatilityScore,
+        user.riskLevel
+      );
+
+      ResponseService.ok(res, { recommendations });
+    } catch (error) {
+      ResponseService.internalError(
+        res,
+        error,
+        "Error al generar recomendaciones"
+      );
     }
+  }
 }
